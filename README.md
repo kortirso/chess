88b451162857bf1938847153a3ae9249839bdb5c
kortirso <kortirso@gmail.com>
2019-01-18 15:44:00 +0300

    release 0.3.4

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ by adding `chess` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:chess, "~> 0.3.3"}
+    {:chess, "~> 0.3.4"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Chess.MixProject do
   def project do
     [
       app: :chess,
-      version: "0.3.3",
+      version: "0.3.4",
       elixir: "~> 1.7",
       name: "Chess",
       description: @description,
